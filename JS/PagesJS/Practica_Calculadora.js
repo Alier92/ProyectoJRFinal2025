a187d80264e79dbf701a2838579a4e2992f817e2
Alier92 <alierquesadaq@vlacademy.com>
2025-02-03 20:13:40 -0600

    datos y suma

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -7,3 +7,16 @@ console.log(`Funcionando Archivo: Practica_Calculadora`);
 
 }
 
+
+console.log('mostrar Nombre:',nombre);
+console.log('Mostrar edad:',edad);
+
+alert (`Nombre ${nombre} edad ${edad}`)
+
+function realizarSuma() {
+    let numero1 = getElementById('numero1').value;
+    let numero2 = getElementById('numero2').value;
+
+    console.log(numero1)
+    console.log(numero2)
+}

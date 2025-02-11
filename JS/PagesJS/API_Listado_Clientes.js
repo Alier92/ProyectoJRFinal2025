c96e05eb8d1215db11502f01eca35baae2d07acc
Alier92 <alierquesadaq@vlacademy.com>
2025-02-10 20:35:39 -0600

    consumir Api y guardar datos en variable

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -2,31 +2,38 @@ console.log('Funcionando Archivo: API_Listado_Clientes');
 
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
-let Datos_Locales = [];
+
+// Datos locales simulando la API
+
+
 
 let DatosFiltrados = []
 let contenedor = document.getElementById('tbody');
 
-DatosFiltrados = Datos_Locales
 
+//aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
-    Mostrar_CampoTabla()
+    consultarDatos()
+
 });
 
-
+// Consultar Datos del API mediante Fetch(AJAX) AXIOS
 function consultarDatos() {
 
-        //fetchget
-        fetch(Api_Usuarios)
-            
-            .then(response => response.json())
-            .then(result => {
-                // custom error
-                console.log(result)
-            })
-            .catch(error => {
-                // common error
-                return null;
-            });
+    //fetchget
+    fetch(Api_Usuarios) //1- se le indica la url de la api 
         
-    }
+        .then(response => response.json()) // 2 -Conversion de datos , JSON 
+        .then(result => {           //3- Resultado  result
+            // custom error
+            console.log(result)
+            DatosFiltrados = result
+        })
+        .catch(error => {      ///4- Catch
+            // common error
+            alert("Error ",error)
+            return null;
+        });
+    
+}
+  
